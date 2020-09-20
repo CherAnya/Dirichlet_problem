b4af92d049b86072880ee5f78d348791facaaa16
CherAnya <62787374+CherAnya@users.noreply.github.com>
2020-09-20 14:15:14 +0500

    Add files via upload

---
+++ MetodOpenMP.c
@@ -0,0 +1,240 @@
+﻿#include <omp.h>
+#include <stdio.h>
+#include <math.h>
+#define E 0.001
+#define N 512
+#define w 1.99
+
+double present[N + 1][N + 1];
+double grid[N + 1][N + 1];
+double X[N + 1];
+double past[N + 1][N + 1];
+
+double f(double a, double b) {
+	double fr = 0;
+	fr = 4 + 2 * pow(a, 2) - 2 * a + 2 * pow(b, 2) - 2 * b;
+	return fr;
+}
+
+double max(double a, double b) {
+	double m = 0;
+	if (a > b)
+		m = a;
+	else
+		m = b;
+	return m;
+}
+
+double u(double a, double b) {
+	double c = 0;
+	c = (pow(a, 2) - a + 1) * (pow(b, 2) - b + 1);
+	return c;
+}
+
+int main(void) {
+	double start_time, end_time;
+	double h = 1.0 / N;
+	int iter = 0;
+
+	start_time = omp_get_wtime();
+	for (int i = 0; i < N + 1; i++) {
+		X[i] = h * i;
+	}
+
+	for (int i = 0; i < N + 1; i++) {
+		for (int j = 0; j < N + 1; j++) {
+			grid[i][j] = u(X[j], X[i]);
+			if (i == 0 || j == 0 || i == N || j == N) {
+				present[i][j] = grid[i][j];
+				past[i][j] = present[i][j];
+			}
+		}
+	}
+
+	double maxe;
+
+
+	//3 метод
+
+/*	do {
+                int i;
+                int j;
+		maxe = 0;
+#pragma omp parallel private(i) num_threads(4)
+		{
+#pragma omp for schedule (static)
+		for (i = 1; i < N; i++) {
+			if (i % 2 == 1) {
+#pragma omp parallel private(j) num_threads(4)
+		{
+#pragma omp for schedule (static)
+				for (j = 1; j < N; j += 2) {
+					present[i][j] = (w / 4.0) * (present[i - 1][j] + past[i + 1][j] + present[i][j - 1] + past[i][j + 1] - pow(h, 2) * f(X[j], X[i])) + (1 - w) * present[i][j];
+					maxe = max(maxe, fabs(past[i][j] - grid[i][j]));
+					past[i][j] = present[i][j];
+				}
+			}
+        }
+			if (i % 2 == 0) {
+#pragma omp parallel private(j) num_threads(4)
+		{
+#pragma omp for schedule (static)
+				for (j = 2; j < N; j += 2) {
+					present[i][j] = (w / 4.0) * (present[i - 1][j] + past[i + 1][j] + present[i][j - 1] + past[i][j + 1] - pow(h, 2) * f(X[j], X[i])) + (1 - w) * present[i][j];
+					maxe = max(maxe, fabs(past[i][j] - grid[i][j]));
+					past[i][j] = present[i][j];
+				}
+			}
+		}
+    }
+}
+#pragma omp parallel private(i) num_threads(4)
+		{
+#pragma omp for schedule (static)
+		for (i = 1; i < N; i++) {
+			if (i % 2 == 1) {
+#pragma omp parallel private(j) num_threads(4)
+		{
+#pragma omp for schedule (static)
+				for (j = 2; j < N; j += 2) {
+					present[i][j] = (w / 4.0) * (present[i - 1][j] + past[i + 1][j] + present[i][j - 1] + past[i][j + 1] - pow(h, 2) * f(X[j], X[i])) + (1 - w) * present[i][j];
+					maxe = max(maxe, fabs(past[i][j] - grid[i][j]));
+					past[i][j] = present[i][j];
+				}
+			}
+                }
+			if (i % 2 == 0) {
+#pragma omp parallel private(j) num_threads(4)
+		{
+#pragma omp for schedule (static)
+				for (j = 1; j < N; j += 2) {
+					present[i][j] = (w / 4.0) * (present[i - 1][j] + past[i + 1][j] + present[i][j - 1] + past[i][j + 1] - pow(h, 2) * f(X[j], X[i])) + (1 - w) * present[i][j];
+					maxe = max(maxe, fabs(past[i][j] - grid[i][j]));
+					past[i][j] = present[i][j];
+				}
+			}
+		}
+    }
+}
+		iter++;
+		printf("%d %8.7f\n", iter, maxe);
+	} while (maxe >= E);
+	end_time = omp_get_wtime();*/
+
+	// 2 метод
+		do {
+        int i;
+		int j;
+		maxe = 0;
+#pragma omp parallel private(i) num_threads(4)
+		{
+#pragma omp for schedule (static)
+		for (i = 1; i < N; i++) {
+			if (i % 2 == 1) {
+#pragma omp parallel private(j) num_threads(4)
+				{
+#pragma omp for schedule (static)
+				for (j = 1; j < N; j += 2) {
+					present[i][j] = (present[i - 1][j] + past[i + 1][j] + present[i][j - 1] + past[i][j + 1] - pow(h, 2) * f(X[j], X[i])) / 4.0;
+					maxe = max(maxe, fabs(past[i][j] - grid[i][j]));
+					past[i][j] = present[i][j];
+				}
+			}
+        }
+			if (i % 2 == 0) {
+#pragma omp parallel private(j) num_threads(4)
+				{
+#pragma omp for schedule (static)
+				for (j = 2; j < N; j += 2) {
+					present[i][j] = (present[i - 1][j] + past[i + 1][j] + present[i][j - 1] + past[i][j + 1] - pow(h, 2) * f(X[j], X[i])) / 4.0;
+					maxe = max(maxe, fabs(past[i][j] - grid[i][j]));
+					past[i][j] = present[i][j];
+				}
+			}
+		}
+    }
+}
+#pragma omp parallel private(i) num_threads(4)
+		{
+#pragma omp for schedule (static)
+		for (i = 1; i < N; i++) {
+			if (i % 2 == 1) {
+#pragma omp parallel private(j) num_threads(4)
+				{
+#pragma omp for schedule (static)
+				for (j = 2; j < N; j += 2) {
+					present[i][j] = (present[i - 1][j] + past[i + 1][j] + present[i][j - 1] + past[i][j + 1] - pow(h, 2) * f(X[j], X[i])) / 4.0;
+					maxe = max(maxe, fabs(past[i][j] - grid[i][j]));
+					past[i][j] = present[i][j];
+				}
+			}
+                }
+			if (i % 2 == 0) {
+#pragma omp parallel private(j) num_threads(4)
+				{
+#pragma omp for schedule (static)
+				for (j = 1; j < N; j += 2) {
+					present[i][j] = (present[i - 1][j] + past[i + 1][j] + present[i][j - 1] + past[i][j + 1] - pow(h, 2) * f(X[j], X[i])) / 4.0;
+					maxe = max(maxe, fabs(past[i][j] - grid[i][j]));
+					past[i][j] = present[i][j];
+				}
+			}
+		}
+    }
+}
+		iter++;
+		printf("%d %8.7f\n", iter, maxe);
+	} while (maxe >= E);
+	end_time = omp_get_wtime();
+
+	// 1 метод
+	/*do {
+		int i;
+		int j;
+		maxe = 0;
+#pragma omp parallel private(i) num_threads(2)
+		{
+#pragma omp for schedule (static)
+			for (i = 1; i < N; i++) {
+#pragma omp parallel private(j) num_threads(2)
+				{
+#pragma omp for schedule (static)
+					for (j = 1; j < N; j++) {
+						present[i][j] = (past[i - 1][j] + past[i + 1][j] + past[i][j - 1] + past[i][j + 1] - pow(h, 2) * f(X[j], X[i])) / 4.0;
+						maxe = max(maxe, fabs(past[i][j] - grid[i][j]));
+					}
+				}
+			}
+		}
+#pragma omp parallel private(i) num_threads(2)
+		{
+#pragma omp for schedule (static)
+			for (i = 1; i < N; i++) {
+#pragma omp parallel private(j) num_threads(2)
+				{
+#pragma omp for schedule (static)
+					for (j = 1; j < N; j++) {
+						past[i][j] = present[i][j];
+					}
+				}
+			}
+		}
+		iter++;
+		printf("%d %8.7f\n", iter, maxe);
+	} while (maxe >= E);
+	end_time = omp_get_wtime();*/
+
+
+	/*	FILE* fout;
+		fout = fopen("Out.txt", "w");
+		for (int i = 1; i < N; i++)
+			for (int j = 1; j < N; j++) {
+				fprintf(fout, "%5.4f\t %5.4f\t %5.4f\t\n", X[j], X[i], present[i][j]);
+			}
+		fclose(fout);*/
+	printf("Number of iterations:\n");
+	printf("%d\n", iter);
+	printf("Time, s:\n");
+	printf("%4.2lf\n", end_time - start_time);
+	return 0;
+}
---
+++ kernel.cu
@@ -0,0 +1,167 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <malloc.h>
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+
+#define E 0.001
+#define N  (512*512)
+#define BLOCKS 256
+
+__global__ void Kernel(double* present, double* past, double* grid, double* f, int SIZE)
+{
+	double h = 1.0 / (SIZE - 1);
+	int id = blockIdx.x * blockDim.x + threadIdx.x;
+
+	if (id >= SIZE && id <= (SIZE * SIZE - SIZE - 1) && id % SIZE != 0 && (id + 1) % SIZE != 0)
+		present[id] = 0.25 * (past[id - SIZE] + past[id - 1] + past[id + SIZE] + past[id + 1] - pow(h, 2) * f[id]);
+	else
+		present[id] = past[id];
+}
+
+__global__ void Errors(double* present, double* grid, double* error)
+{
+	int id = blockIdx.x * blockDim.x + threadIdx.x;
+	error[id] = fabs(present[id] - grid[id]);
+}
+
+double fr(double a, double b) {
+	return 4 + 2 * pow(a, 2) - 2 * a + 2 * pow(b, 2) - 2 * b;
+}
+
+double u(double a, double b) {
+	double c = 0;
+	c = (pow(a, 2) - a + 1) * (pow(b, 2) - b + 1);
+	return c;
+}
+
+void gran(double* arr, double* Z, int X0, int Xn, int Y0, int Yn, int SIZE)
+{
+	int k = 0;
+	for (int i = X0; i < Xn; ++i)
+	{
+		for (int j = Y0; j < Yn; ++j)
+		{
+			k = i - X0 + j - Y0;
+			arr[i * SIZE + j] = Z[k] * Z[k] - Z[k] + 1;
+		}
+	}
+
+}
+
+bool Writetofile(char* filename, double* X, double* Y, double* Z, int SIZE)
+{
+
+	FILE* fp = fopen(filename, "w");
+	if (fp == NULL)
+	{
+		printf("Can not open file %s\n", filename);
+		return false;
+	}
+	for (int i = 0; i < SIZE; i++)
+		for (int j = 0; j < SIZE; j++)
+			fprintf(fp, "%7.8f %7.8f %7.8f\n", X[i], Y[j], Z[i * SIZE + j]);
+	fclose(fp);
+	return true;
+}
+
+int main()
+{
+	int SIZE = 512;
+	int steperror = 1000;
+	double* present = (double*)malloc((SIZE * SIZE) * sizeof(double));
+	double* grid = (double*)malloc((SIZE * SIZE) * sizeof(double));
+	double* past = (double*)malloc((SIZE * SIZE) * sizeof(double));
+
+
+	double* X = (double*)malloc(SIZE * sizeof(double));
+	double* Y = (double*)malloc(SIZE * sizeof(double));
+	double* f = (double*)malloc(SIZE * SIZE * sizeof(double));
+	double* errors = (double*)malloc(SIZE * SIZE * sizeof(double));
+
+	for (int i = 0; i < SIZE; i++) {
+		X[i] = double(i) / (SIZE - 1);
+		Y[i] = double(i) / (SIZE - 1);
+	}
+
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; ++j) {
+			past[i * SIZE + j] = 0;
+			grid[i * SIZE + j] = u(X[i], Y[j]);
+			f[i * SIZE + j] = fr(X[i], Y[j]);
+		}
+	}
+
+	clock_t start, end;
+	double time;
+
+	start = clock();
+
+	gran(past, X, 0, SIZE, 0, 1, SIZE);
+	gran(past, X, 0, SIZE, SIZE - 1, SIZE, SIZE);
+	gran(past, Y, 0, 1, 0, SIZE, SIZE);
+	gran(past, Y, SIZE - 1, SIZE, 0, SIZE, SIZE);
+
+	double localMax = 1;
+	int iter = 0;
+
+	double* dev_arrpresent = 0;
+	double* dev_arrpast = 0;
+	double* dev_arrgrid = 0;
+	double* dev_f = 0;
+	double* dev_error = 0;
+
+	cudaSetDevice(0);
+	cudaMalloc((void**)&dev_arrpresent, N * sizeof(double));
+	cudaMalloc((void**)&dev_arrpast, N * sizeof(double));
+	cudaMalloc((void**)&dev_arrgrid, N * sizeof(double));
+	cudaMalloc((void**)&dev_f, N * sizeof(double));
+	cudaMalloc((void**)&dev_error, N * sizeof(double));
+
+	cudaMemcpy(dev_arrpast, past, N * sizeof(double), cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_arrgrid, grid, N * sizeof(double), cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_f, f, N * sizeof(double), cudaMemcpyHostToDevice);
+
+	while (localMax > E)
+	{
+		Kernel << < BLOCKS, N / BLOCKS >> > (dev_arrpresent, dev_arrpast, dev_arrgrid, dev_f, SIZE);
+		if (iter % steperror == 0) {
+			localMax = 0;
+			Errors << < BLOCKS, N / BLOCKS >> > (dev_arrpresent, dev_arrgrid, dev_error);
+			cudaMemcpy(errors, dev_error, N * sizeof(double), cudaMemcpyDeviceToHost);
+			for (int i = 0; i < SIZE * SIZE; i++)
+				if (localMax < errors[i])
+					localMax = errors[i];
+			printf("%d, %lf\n", iter, localMax);
+		}
+		double* temp = dev_arrpast;
+		dev_arrpast = dev_arrpresent;
+		dev_arrpresent = temp;
+		iter++;
+	}
+	cudaMemcpy(past, dev_arrpresent, N * sizeof(double), cudaMemcpyDeviceToHost);
+	Writetofile("Jakobi.dat", X, Y, past, SIZE);
+
+	end = clock();
+	time = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+	printf("Number of iterations:\n");
+	printf("%d\n", iter - 1);
+	printf("Time, s:\n");
+	printf("%4.2lf\n", time);
+
+	cudaFree(dev_arrpresent);
+	cudaFree(dev_arrpast);
+	cudaFree(dev_arrgrid);
+	cudaFree(dev_f);
+	cudaFree(dev_error);
+
+	free(X);
+	free(Y);
+	free(present);
+	free(grid);
+	free(past);
+
+	return 0;
+}
